1ac5ff49b31cf9bcabb6fa5550f5e7e0560a3daf
BerryBC <Berry_BC_@163.com>
2020-02-05 22:38:54 +0800

    更新配置让所有人都可以访问

--- SpyDataWebAppAndAPI/settings.py
+++ SpyDataWebAppAndAPI/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'rf^qfp%v%_6b*=(s&lb&529g9!dr8zyw=8#nd(-@vg$wgdfsz@'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1']
+ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1','*']
 
 
 # Application definition
