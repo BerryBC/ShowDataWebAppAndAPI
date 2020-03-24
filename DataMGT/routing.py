50150aadfa08a85f87e809cfd912e4c5ae9b5e0c
BerryBC <Berry_BC_@163.com>
2020-03-24 23:04:56 +0800

    全部注释

--- DataMGT/routing.py
+++ DataMGT/routing.py
@@ -3,12 +3,12 @@
 @Author: BerryBC
 @Date: 2020-02-24 23:03:32
 @LastEditors: BerryBC
-@LastEditTime: 2020-02-25 00:03:24
+@LastEditTime: 2020-03-24 23:04:14
 '''
-from django.urls import re_path
+# from django.urls import re_path
 
-from . import consumers
+# from . import consumers
 
-websocket_urlpatterns = [
-    re_path('ws/data/sklcws/', consumers.wsCreatSklearnModel),
-]
+# websocket_urlpatterns = [
+#     re_path('ws/data/sklcws/', consumers.wsCreatSklearnModel),
+# ]
