c51f0a9c7a843993fcdaafa0ec3b5a42433fd2e3
BerryBC <Berry_BC_@163.com>
2020-03-29 15:44:14 +0800

    继续更新一些小 Bug

--- static/data/js/sklearnmodelc.js
+++ static/data/js/sklearnmodelc.js
@@ -3,7 +3,7 @@
  * @Author: BerryBC
  * @Date: 2020-02-23 10:38:36
  * @LastEditors: BerryBC
- * @LastEditTime: 2020-03-29 15:35:06
+ * @LastEditTime: 2020-03-29 15:44:03
  */
 $(function() {
     $('#btnCreat')[0].onclick = function() {
@@ -34,13 +34,6 @@ $(function() {
                     $('#taContent').val($('#taContent').val() + ' Done creat classification\n');
                     wsSocks.close();
                 };
-
-                $('#taContent').val($('#taContent').val() + received_msg + '\n');
-                setTimeout(() => {
-                    wsSocks.send(JSON.stringify({
-                        'message': 1
-                    }));
-                }, 1000);
             };
 
             wsSocks.onclose = function() {
