b356759881c338d7a43f35fe7865bba3219d126e
BerryBC <Berry_BC_@163.com>
2020-05-31 13:57:51 +0800

    更新饼图

--- static/data/js/piechart.js
+++ static/data/js/piechart.js
@@ -3,7 +3,7 @@
  * @Author: BerryBC
  * @Date: 2020-05-21 22:13:31
  * @LastEditors: BerryBC
- * @LastEditTime: 2020-05-31 13:14:27
+ * @LastEditTime: 2020-05-31 13:57:11
  */
 var gobjData = {};
 var chartPie = echarts.init(document.getElementById('divPieChart'));
@@ -40,76 +40,8 @@ $(function() {
     };
 
 
-    $('#selKW').change(function() {
-        if ($('#selKW').val() == "-1") {
-            ShowModal(false, 'None record for this keyword');
-        } else {
-            optPie = {
-                title: {
-                    text: $('#selKW').val(),
-                    left: 'center',
-                    top: 'center',
-                    textStyle: {
-                        fontSize: 38
-
-                    }
-                },
-                tooltip: {
-                    trigger: 'item',
-                    formatter: '{a} <br/>{b}: {c} ({d}%)'
-                },
-
-                series: [{
-                    name: '词语好坏',
-                    type: 'pie',
-                    hoverAnimation: true,
-                    radius: ['40%', '70%'],
-                    avoidLabelOverlap: false,
-                    labelLine: {
-                        show: false
-                    },
-                    data: [
-                        { value: 0, name: '正面文章', itemStyle: { color: "#91c7ae" } },
-                        { value: 0, name: '反面文章', itemStyle: { color: "#c23531" } },
-                        { value: 0, name: '无意义文章', itemStyle: { color: "#2f4554" } }
-                    ]
-                }]
-            };
-            chartPie.setOption(optPie);
-            setTimeout(() => {
-                optPie = {
-                    title: {
-                        text: $("#selKW").val(),
-                        left: 'center',
-                        top: "center",
-                        textStyle: {
-                            fontSize: 38
-                        }
-                    },
-                    tooltip: {
-                        trigger: 'item',
-                        formatter: '{a} <br/>{b}: {c} ({d}%)'
-                    },
-                    series: [{
-                        name: '词语好坏',
-                        type: 'pie',
-                        hoverAnimation: true,
-                        radius: ['40%', '70%'],
-                        avoidLabelOverlap: false,
-                        labelLine: {
-                            show: false
-                        },
-                        data: [
-                            { value: gobjData[$("#selKW").val()][2], name: '正面文章', itemStyle: { color: "#91c7ae" } },
-                            { value: gobjData[$("#selKW").val()][0], name: '反面文章', itemStyle: { color: "#c23531" } },
-                            { value: gobjData[$("#selKW").val()][1], name: '无意义文章', itemStyle: { color: "#2f4554" } }
-                        ]
-                    }]
-                };
-                chartPie.setOption(optPie);
-            }, 100);
-        };
-    });
+    $('#selKW').change(function() { SelectKW() });
+    $('#selKW').click(function() { SelectKW() });
 });
 
 function InsertSelector(arrKWs) {
@@ -125,5 +57,80 @@ function InsertSelector(arrKWs) {
     } else {
         ShowModal(false, 'None record for this keyword');
         selKW.append('<option value="-1" selected>Non of this Key Word in database</option>');
+    };
+};
+
+function SelectKW() {
+
+    if ($('#selKW').val() == "-1") {
+        ShowModal(false, 'None record for this keyword');
+    } else {
+        optPie = {
+            title: {
+                text: $('#selKW').val(),
+                left: 'center',
+                top: 'center',
+                textStyle: {
+                    fontSize: 38
+                }
+            },
+            tooltip: {
+                trigger: 'item',
+                formatter: '{a} <br/>{b}: {c} ({d}%)'
+            },
+
+            series: [{
+                name: '词语好坏',
+                type: 'pie',
+                hoverAnimation: true,
+                radius: ['40%', '70%'],
+                avoidLabelOverlap: false,
+                label: {
+                    position: 'outer',
+                    alignTo: 'labelLine',
+                    bleedMargin: 5
+                },
+                data: [
+                    { value: 0, name: '正面文章', itemStyle: { color: "#91c7ae" } },
+                    { value: 0, name: '反面文章', itemStyle: { color: "#c23531" } },
+                    { value: 0, name: '无意义文章', itemStyle: { color: "#2f4554" } }
+                ]
+            }]
+        };
+        chartPie.setOption(optPie);
+        setTimeout(() => {
+            optPie = {
+                title: {
+                    text: $("#selKW").val(),
+                    left: 'center',
+                    top: "center",
+                    textStyle: {
+                        fontSize: 38
+                    }
+                },
+                tooltip: {
+                    trigger: 'item',
+                    formatter: '{a} <br/>{b}: {c} ({d}%)'
+                },
+                series: [{
+                    name: '词语好坏',
+                    type: 'pie',
+                    hoverAnimation: true,
+                    radius: ['40%', '70%'],
+                    avoidLabelOverlap: false,
+                    label: {
+                        position: 'outer',
+                        alignTo: 'labelLine',
+                        bleedMargin: 5
+                    },
+                    data: [
+                        { value: gobjData[$("#selKW").val()][2], name: '正面文章', itemStyle: { color: "#91c7ae" } },
+                        { value: gobjData[$("#selKW").val()][0], name: '反面文章', itemStyle: { color: "#c23531" } },
+                        { value: gobjData[$("#selKW").val()][1], name: '无意义文章', itemStyle: { color: "#2f4554" } }
+                    ]
+                }]
+            };
+            chartPie.setOption(optPie);
+        }, 100);
     };
 };
